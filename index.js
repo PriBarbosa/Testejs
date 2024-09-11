0d372c4f1a1f6572980f9367ff05658ba67280b5
Priscila Barbosa <priscilabarbosa1510@gmail.com>
2024-09-11 14:43:38 -0300

    criar nova pasta para exemplos Andressa. Adicionei os meses.

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-// const numero1 = 5
-// var numero2 = 2
-
-let numero1 = prompt("Digite um numero:")
-let numero2 = prompt("Digite outro numero:")
-let operacao = prompt("Digite a operação: +, -, /, , *, %")
-
-let soma = parseInt(numero1) + parseInt(numero2);
-let subtracao = numero1 - numero2;
-let divisao = numero1 / numero2;
-let multiplicacao = numero1 * numero2;
-let expoente = numero1 ** numero2;
-let resto = numero1 % 2;
-
-if (operacao == '+') {
-    console.log(soma)
-}
-if (operacao == '-') {
-    console.log(subtracao)
-}
-if (operacao == '/') {
-    console.log(divisao)
-}
-if (operacao == '*') {
-    console.log(multiplicacao)
-}
-if (operacao == '**') {
-    console.log(expoente)
-}
-if (operacao == '%') {
-    console.log(resto)
-
-    if (resto == 0) {
-        console.log('é par')
-    }
-    else {
-        console.log('é impar')
-    }
-}
+let numero1 = prompt("Qual mês você faz aniversário?")
 
 
-// alert(soma)
-// console.log(operacao)
-// console.log(subtracao)
-// console.log(divisao)
-// console.log(multiplicacao)
+let MES = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
+    "Outubro", "Novembro", "Dezembro"];
+
+
+let Janeiro = 0
+let Fevereiro = 1
+let Março = 2
+let Abril = 3
+
+
+
