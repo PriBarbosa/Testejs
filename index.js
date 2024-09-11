1f5aba51eed3fd10ba2a0d5b34821407996cea26
Priscila Barbosa <priscilabarbosa1510@gmail.com>
2024-09-11 19:55:15 -0300

    mudaça nome da pasta/ construção do repositorio

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ let Janeiro = 0
 let Fevereiro = 1
 let Março = 2
 let Abril = 3
+let Maio = 4
+let Junho = 5
+let Julho = 6
+let Agosto = 7
+let Setembro = 8
+let Outubro = 9
+let Novembro = 10
+let Dezembro = 12
+
+
+
+if (numero1 == 0)
+    console.log('janeiro')
+else if (numero1 == 1);
+    console.log('fevereiro')
+if (numero1 == 1)
+    console.log('fervereiro')
+else if (numero1 == 2)
+    console.log('março');
+if(numero1 == 3)
+    console.log('março')
+
+
+
 
 
 
