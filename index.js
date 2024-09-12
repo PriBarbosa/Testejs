a346d08f8805483a2e5e55be373e73fd1362791a
Priscila Barbosa <priscilabarbosa1510@gmail.com>
2024-09-12 14:29:10 -0300

    Complente os "meses" de janeiro a dezembro com if e else.

--- index.js
+++ index.js
@@ -5,33 +5,69 @@ let MES = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
     "Outubro", "Novembro", "Dezembro"];
 
 
-let Janeiro = 0
-let Fevereiro = 1
-let Março = 2
-let Abril = 3
-let Maio = 4
-let Junho = 5
-let Julho = 6
-let Agosto = 7
-let Setembro = 8
-let Outubro = 9
-let Novembro = 10
+let Janeiro = 1
+let Fevereiro = 2
+let Março = 3
+let Abril = 4
+let Maio = 5
+let Junho = 6
+let Julho = 7
+let Agosto = 8
+let Setembro = 9
+let Outubro = 10
+let Novembro = 11
 let Dezembro = 12
 
 
 
-if (numero1 == 0)
-    console.log('janeiro')
-else if (numero1 == 1);
-    console.log('fevereiro')
 if (numero1 == 1)
-    console.log('fervereiro')
+    console.log('janeiro')
 else if (numero1 == 2)
-    console.log('março');
-if(numero1 == 3)
+    console.log('fevereiro')
+if (numero1 == 2)
+    console.log('fervereiro')
+else if (numero1 == 3)
     console.log('março')
-
-
+if (numero1 == 3)
+    console.log('março')
+else if (numero1 == 4)
+    console.log('abril')
+if (numero1 == 4)
+    console.log('abril')
+else if (numero1 == 5)
+    console.log('maio')
+if (numero1 == 5)
+    console.log('maio')
+else if (numero1 == 6)
+    console.log('junho')
+if (numero1 == 6)
+    console.log('junho')
+else if (numero1 == 7)
+    console.log("julho")
+if (numero1 == 7)
+    console.log('julho')
+else if (numero1 == 8)
+    console.log('agosto')
+if (numero1 == 8)
+    console.log("agosto")
+else if (numero1 == 9)
+    console.log('setembro')
+if (numero1 == 9)
+    console.log('setembro')
+else if (numero1 == 10)
+    console.log('outubro')
+if (numero1 == 10)
+    console.log("outubro")
+else if (numero1 == 11)
+    console.log('novembro')
+if (numero1 == 11)
+    console.log(numero1 == 12)
+else if (numero1 == 12)
+    console.log('dezembro')
+if (numero1 == 12)
+    console.log('dezembro')
+else if (numero1 == 13)
+    console.log('janeiro')
 
 
 
