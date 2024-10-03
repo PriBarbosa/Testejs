84c4f0afd8fbed27e5c52099a91c04d56c99e03c
Priscila Barbosa <priscilabarbosa1510@gmail.com>
2024-10-03 19:19:23 -0300

    criação da função innerText para o resultado apareça na tela

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-let soma = parseInt(number1) + parseInt(number2);
-let subtracao = (number1) - (number2);
-let divisao = (number1) / (number2);
-let multiplicacao = (number1) * (number2);
-
-
-
-
-
 function calcular() {
     let nu1 = document.getElementById('number1')
     let nu2 = document.getElementById('number2')
-    let operacao = document.getElementById('operação')
+    let operacao = document.getElementById('operacao').value
+    console.log(operacao)
     let num1 = nu1.value
     let num2 = nu2.value
-    let resultado = (num1) * (num2)
+    let resultado = 0
+    let resultadoTela =document.getElementById("resultado")
 
     if (operacao == "*") {
         resultado = num1 * num2;
@@ -25,12 +18,13 @@ function calcular() {
         resultado = num1 / num2;
     }
     else if (operacao == '+') {
-        resultado = num1  + num2
+        resultado = parseInt(num1) + parseInt(num2)
     }
-    
+
+
     console.log(resultado)
 
-    alert(resultado)
 
 
+ resultadoTela.innerText= resultado
 }
